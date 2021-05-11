d7c6414b0fe2963e92b8cee47877fd2ee0cabb88
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-11 13:49:37 -0600

    license images/links

--- Develop/README.md
+++ Develop/README.md
@@ -1,5 +1,5 @@
-# kjwbfn;ewdn
-  # ;wfhub;qwoikn
-  # license: [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-      
+# prot
+  # eofvknme'f
+  # license: [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    
 
--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,20 @@
-// TODO: Include packages needed for this application
+// TODO: PACKAGES NEEDED FOR APPLICATION:
 
-//GRAB FROM GENERATE MARKDOWN FILE
-const generateMarkdown = require("./utils/generateMarkdown");  
-//GRAB INQUIRER PACKAGE FROM PACKAGE.JSON
-const inquirer = require("inquirer");
-//BUILT IN NODE PACKAGE
-const fs = require("fs");
+  //GRAB FROM GENERATE MARKDOWN FILE
+    const generateMarkdown = require("./utils/generateMarkdown");  
+  //GRAB INQUIRER PACKAGE FROM PACKAGE.JSON
+    const inquirer = require("inquirer");
+  //BUILT IN NODE PACKAGE
+    const fs = require("fs");
+
+
+
+
+
+
+
+
+
 
 
 // TODO: Create an array of questions for user input
@@ -25,7 +34,7 @@ inquirer.prompt ([
     type: "list",
     name: "license",
     message: "What license does your app use? (use arrows)",
-    choices: ["Mozilla", "MIT", "Appache"]
+    choices: ["Mozilla", "MIT", "Apache"]
   }
 
 ]).then (response => {
@@ -40,3 +49,21 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
