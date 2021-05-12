3c07bc4cdda6f5e7c9da1f0847c297dc7e819b7d
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-11 23:24:58 -0600

    testing markdown

--- Develop/README.md
+++ Develop/README.md
@@ -1,5 +1,5 @@
-# prot
-  # eofvknme'f
+# README Generator
+  # cpesar1@gmail.com
   # license: [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-    
+  # licenseLink: https://www.apache.org/licenses/LICENSE-2.0.txt
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,28 +11,33 @@ function renderLicenseBadge(license) {
   } else (license === "Apache")
     return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
 
-  return '';
+  
+  if (!license) {
+    return '';
+  }
 };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === "Mozilla"){
+function renderLicenseLink(licenseLink) {
+  if (licenseLink === "Mozilla"){
     return `https://www.mozilla.org/en-US/MPL/2.0/FAQ/`
 
-  } else if (license === "MIT"){
+  } else if (licenseLink === "MIT"){
     return `https://opensource.org/licenses/MIT`
 
-  } else (license === "Apache")
+  } else (licenseLink === "Apache")
   return 'https://www.apache.org/licenses/LICENSE-2.0.txt'
 
-   return ''; 
+  if (!licenseLink) {
+    return '';
+  } 
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+
 }
 
 // TODO: Create a function to generate markdown for README
@@ -40,6 +45,7 @@ function generateMarkdown(response) {
   return `# ${response.title}
   # ${response.email}
   # license: ${renderLicenseBadge(response.license)}
+  # licenseLink: ${renderLicenseLink(response.licenseLink)}
 
 `;
 }
